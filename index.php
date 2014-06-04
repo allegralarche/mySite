c9de7910e8007e5ba5504277a539996e511b924a
Allegra <allegralarche@gmail.com>
2014-06-03 23:47:17 -0400

    initial layout html

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-<?php echo "Hello World!"?>
+<div class="header">
+	<h1>Allegra Larche</h1>
+</div>
+<div class="nav">
+	<ul>
+		<li>About</li>
+		<li>Past Projects</li>
+		<li>Resume</li>
+		<li>Contact Me</li>
+	</ul>
+</div>
+<div class="about-section">
+</div>
+<div class="past-projects-section">
+</div>
+<div class="resume-section">
+</div>
+<div class="contact-me-section">
+</div>
